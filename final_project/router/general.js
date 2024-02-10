9f4d78ed9541f9518bb1f762a9824206023bb533
JustCallMeOpen <159606248+JustCallMeOpen@users.noreply.github.com>
2024-02-10 13:11:04 -0600

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,25 +13,52 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
+  res.send(JSON.stringify(books, null, 4));
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    if (books[isbn]) {
+        return res.json(books[isbn]);
+    } else {
+  return res.status(404).json({message: "Book not found"});
+ }
+ // return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;  
+    const authorBooks = [];  
+    
+    for (const book in books) {  
+      if (books[book].author === author) {  
+        authorBooks.push(books[book]);
+      }
+    }
+    
+    if (authorBooks.length > 0) {  
+      res.send(authorBooks);  
+    } else {
+      res.status(404).send('No books found for author');  
+    }
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title.toLowerCase();
+  const filteredBooks = Object.values(books).filter(book => book.title.toLowerCase().includes(title));
+  if(filteredBooks.length > 0){
+      return res.status(200).json(filteredBooks);
+  }
+  else{
+      return res.status(404).json({message: "Book not found"});
+  }
+  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
