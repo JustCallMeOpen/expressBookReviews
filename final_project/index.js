e19f3b902557f3708bb1a6fcfb69b001251ef86b
JustCallMeOpen <159606248+JustCallMeOpen@users.noreply.github.com>
2024-02-11 00:47:32 -0600

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,8 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
+// Authorization Mechanism
 app.use("/customer/auth/*", function auth(req,res,next){
-    //Authentication Mechanism
     if(req.session.authorization) {
         token = req.session.authorization['accessToken'];
         jwt.verify(token, "access",(err,user)=>{
@@ -32,5 +32,3 @@ const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
-
-app.listen(PORT,()=>console.log("Server is running"));
